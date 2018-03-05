7122ec4afc221e31419586562b526b501f91086a
aditya01933 <adityatiwari01933@gmail.com>
2018-03-05 23:38:46 +0530

    Initiate algorithms

---
+++ Rakefile
@@ -0,0 +1,5 @@
+Rake::TestTask.new do |t|
+    t.test_files = FileList['test/**/*_test.rb']
+end
+desc "Run tests"
+task default: :test
---
+++ lib/algorithm.rb
@@ -0,0 +1,5 @@
+Dir[File.join(__dir__, 'algorithm', '**/*.rb')].each {|file| require file }
+
+module Algorithm
+  # Your code goes here...
+end
