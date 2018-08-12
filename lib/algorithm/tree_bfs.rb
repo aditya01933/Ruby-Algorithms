b1e530627d6a8bc31736734bfd54adc0bdd459c1
aditya01933 <adityatiwari01933@gmail.com>
2018-08-12 14:33:00 +0530

    Fix dfs vs bfs

--- lib/algorithm/tree_bfs.rb
+++ lib/algorithm/tree_bfs.rb
@@ -1,16 +1,19 @@
-class TreeDFS
+class TreeBFS
   def run(arr)
     root = Tree::Node.new(arr[0])
     root.left = Tree::Node.new(arr[1])
     root.right = Tree::Node.new(arr[2])
     root.left.left = Tree::Node.new(arr[3])
     root.left.right = Tree::Node.new(arr[4])
-    dfs(root)
+    bfs([root])
   end
 
-  def dfs(root)
-    puts root.value
-    dfs(root.left) if root.left
-    dfs(root.right) if root.right
+  def bfs(q)
+    node = q[0]
+    q << node.left if node.left
+    q << node.right if node.right
+    puts q.shift.value
+    return if q.empty?
+    bfs(q)
   end
 end
